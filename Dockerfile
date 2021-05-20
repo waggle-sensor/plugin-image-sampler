8219b490386bd724c21826b1e26daf8f446f8c1b
Yongho Kim <ironyonghokim@gmail.com>
2021-05-20 00:33:51 -0500

    Added trigger-based image collection

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM waggle/plugin-opencv:4.1.1
-LABEL version="0.1.1" \
+LABEL version="0.2.0" \
       description="Image sampler"
 
 # COPY app.py /app/
--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from datetime import datetime, timezone
 import time
 import os
 import argparse
+import re
 
 import cv2
 
@@ -18,11 +19,64 @@ from waggle.data import open_data_source
 plugin.init()
 
 
-def run(args):
-    print(f"starting image sampler. will sample every {args.interval}s", flush=True)
+def extract_topics(expr):
+    topics = re.findall(r"\b[a-z]\w+", expr)
+    for reserved in ['or', 'and']:
+        while True:
+            try:
+                topics.remove(reserved)
+            except ValueError:
+                break
+    return topics
 
-    if args.out_dir != "":
-        os.makedirs(args.out_dir, exist_ok=True)
+
+def run_on_event(args):
+    print(f'starting image sampler whenever {args.condition} becomes valid', flush=True)
+    topics = {}
+    condition = args.condition.replace('.', '_')
+    for t in extract_topics(condition):
+        topics[t] = 0.
+        plugin.subscribe(t.replace('_', '.'))
+
+    with open_data_source(id=args.stream) as cam:
+        cooldown = time.time()
+        while True:
+            msg = plugin.get()
+            topics[msg.name.replace('.', '_')] = msg.value
+            if time.time() < cooldown:
+                time.sleep(0.1)
+                continue
+
+            if eval(condition, topics):
+                print(f'{args.condition} is valid. getting image', flush=True)
+                try:
+                    ts_ns, image = cam.get(timeout=5)
+                except TimeoutError:
+                    print("get image timed out", flush=True)
+                    continue
+
+                if args.out_dir != "":
+                    # NOTE(YK) We lose nano seconds precision here
+                    dt = datetime.fromtimestamp(ts_ns / 1e9)
+                    path = os.path.join(args.out_dir,
+                                        dt.astimezone(timezone.utc).strftime('%Y-%m-%dT%H:%M:%S%z.jpg'))
+                else:
+                    path = "sample.jpg"
+
+                print("writing image", flush=True)
+                cv2.imwrite(path, image)
+
+                print("uploading image", flush=True)
+                plugin.upload_file(path)
+                
+                print(f'cooling down for {args.cooldown} seconds', flush=True)
+                cooldown = time.time() + args.cooldown
+            else:
+                time.sleep(0.1)
+
+
+def run_periodically(args):
+    print(f"starting image sampler. will sample every {args.interval}s", flush=True)
 
     with open_data_source(id=args.stream) as cam:
         while True:
@@ -45,7 +99,7 @@ def run(args):
 
             print("writing image", flush=True)
             cv2.imwrite(path, image)
-            
+
             print("uploading image", flush=True)
             plugin.upload_file(path)
 
@@ -64,4 +118,20 @@ if __name__ == '__main__':
         '-interval', dest='interval',
         action='store', default=300, type=int,
         help='Inference interval in seconds')
-    run(parser.parse_args())
+    parser.add_argument(
+        '-condition', dest='condition',
+        action='store', default="", type=str,
+        help='Triggering condition')
+    parser.add_argument(
+        '-cooldown', dest='cooldown',
+        action='store', default=5, type=int,
+        help='Cooldown in seconds after a trigger')
+
+    args = parser.parse_args()
+    if args.out_dir != "":
+        os.makedirs(args.out_dir, exist_ok=True)
+
+    if args.condition == "":
+        run_periodically(args)
+    else:
+        run_on_event(args)
